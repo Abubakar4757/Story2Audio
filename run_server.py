80e45917a7e1f7287c48b3b00cea0167aca14c77
Abubakar4757 <abubakar4757@gmail.com>
2025-05-03 18:18:33 +0500

    your message

--- run_server.py
+++ run_server.py
@@ -14,7 +14,6 @@ def serve():
     server.start()
     try:
         while True:
-            print("hello")
             time.sleep(86400)  # 1 day
     except KeyboardInterrupt:
         server.stop(0)
